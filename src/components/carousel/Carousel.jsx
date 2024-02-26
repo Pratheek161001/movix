84952f83ccdfdffd31a113f8ae2b52f946d8b686
Pratheek <pratheeksk70@gmail.com>
2024-02-26 13:29:19 +0530

    crousel done

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,3 +1,4 @@
+
 import React, { useRef } from "react";
 import {
     BsFillArrowLeftCircleFill,
@@ -8,13 +9,71 @@ import { useSelector } from "react-redux";
 import dayjs from "dayjs";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import Img from "../lazyloadimage/img";
+import Img from "../lazyloadimage/img"; 
 import PosterFallback from "../../assets/no-poster.png";
 
 import "./style.scss";
-const Carousel = () => {
+const Carousel = ({ data , loading }) => {
+    const carouselContainer = useRef();
+    const {url}=useSelector((state)=>state.home);
+    const navigate=useNavigate();
+    const navigation =(dir)=>{
+        
+    }
+    const skItem=()=>{
+        return(
+            <div className="skeletonItems">
+                <div className="posterBlock">
+                    <div className="textBlock">
+                        <div className="title skeleton"></div>
+                        <div className="date skeleton"></div>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
   return (
-    <div>Carousel</div>
+    <div className="carousel">
+        <ContentWrapper>
+            <BsFillArrowLeftCircleFill
+                className="carouselLeftNav arrow"
+                onClick={()=>navigation('left')}
+                />
+                <BsFillArrowRightCircleFill
+                className="carouselRighttNav arrow"
+                onClick={()=>navigation('left')}
+                />
+                {!loading ? (
+                    <div className="carouselItems">
+                        {data?.map((item)=>{
+                            const posterUrl = item.poster_path ? url.poster + item.poster_path : PosterFallback;
+                            return(
+                                <div className="carouselItem" key={item.id} >
+                                    <div className="posterBlock">
+                                        <Img src={posterUrl}/>
+                                    </div>
+                                    <div className="textBlock">
+                                        <span className="title">{item.title || item.name}</span>
+                                        <span className="date">{dayjs(item.release_Date).format("MMM D, YYYY")}</span>
+
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                ) : (
+                    <div className="loadingSkeleton">
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                    </div>
+                )}
+        </ContentWrapper>
+    </div>
   )
 }
 
