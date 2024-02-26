191c18c7febd8ac0facccd2ca3f7f37997f9725d
Pratheek <pratheeksk70@gmail.com>
2024-02-26 23:44:02 +0530

    details half

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -11,6 +11,7 @@ import Genres from "../../../components/genres/Genres";
 import CircleRating from "../../../components/circlerating/CircleRating.jsx";
 import Img from "../../../components/lazyloadimage/img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
+import { PlayIcon } from "../Playbtn.jsx";
 
 const DetailsBanner = ({ video, crew }) => {
     const {mediaType, id}=useParams();
@@ -50,6 +51,40 @@ const DetailsBanner = ({ video, crew }) => {
                                         <Genres data={_genres}/>
                                         <div className="row">
                                             <CircleRating rating={data.vote_average.toFixed(1)}/>
+                                            <div className="playbtn" onClick={()=>{}}>
+                                                <PlayIcon/>
+                                            <span className="text">Watch Trailer</span>
+                                            </div>
+                                        </div>
+                                        <div className="overview">
+                                            <div className="heading">Overview</div>
+                                            <div className="description">{data?.overview}</div>
+                                        </div>
+                                        <div className="info">
+                                        {data.status && (
+                                                <div className="infoItem">
+                                                    <span className="text bold">
+                                                        Status:{" "}
+                                                    </span>
+                                                    <span className="text">{data.status}</span>
+                                                </div>
+                                            )}
+                                            {data.release_date && (
+                                                <div className="infoItem">
+                                                    <span className="text bold">
+                                                        Release Date:{" "}
+                                                    </span>
+                                                    <span className="text">{dayjs(data.release_date).format('MMM D, YYYY')}</span>
+                                                </div>
+                                            )}
+                                            {data.runtime && (
+                                                <div className="infoItem">
+                                                    <span className="text bold">
+                                                        Runtime:{" "}
+                                                    </span>
+                                                    <span className="text">{toHoursAndMinutes(data.runtime)}</span>
+                                                </div>
+                                            )}
                                         </div>
                                     </div>
                                 </div>
