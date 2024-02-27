715ad730f003cdacab39863bb79073a28a8d7349
Pratheek <pratheeksk70@gmail.com>
2024-02-27 10:57:49 +0530

    crew

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -12,12 +12,17 @@ import CircleRating from "../../../components/circlerating/CircleRating.jsx";
 import Img from "../../../components/lazyloadimage/img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
 import { PlayIcon } from "../Playbtn.jsx";
+import VideoPopup from "../../../components/videopopup/VideoPopUp.jsx";
 
 const DetailsBanner = ({ video, crew }) => {
+    const [show,setShow] =useState(false);
+    const [videoId,setVideoId]=useState(null);
     const {mediaType, id}=useParams();
     const {data, loading}=useFetch(`/${mediaType}/${id}`)
     const {url}=useSelector((state)=>state.home);
     const _genres = data?.genres?.map((g)=>g.id);
+    const director=crew?.filter((f)=>f.job==='Director');
+    const writer=crew?.filter((f)=>f.job==='Screenplay'||f.job==='Story'||f.job==='Writer')
 
     const toHoursAndMinutes = (totalMinutes) => {
         const hours = Math.floor(totalMinutes / 60);
@@ -86,8 +91,45 @@ const DetailsBanner = ({ video, crew }) => {
                                                 </div>
                                             )}
                                         </div>
+                                        {director.length>0 && (
+                                            <div className="info">
+                                                <span className="text bold">
+                                                    Director:{" "}
+                                                </span>
+                                                <span className="text">
+                                                    {director.map((d, i)=>(
+                                                        <span key={i}>{d.name}{director.length-1 !==i && ", "}</span>
+                                                    ))}
+                                                </span>
+                                            </div>
+                                        )}
+                                        {writer.length>0 && (
+                                            <div className="info">
+                                                <span className="text bold">
+                                                    Writer:{" "}
+                                                </span>
+                                                <span className="text">
+                                                    {writer.map((d, i)=>(
+                                                        <span key={i}>{d.name}{writer.length-1 !==i && ", "}</span>
+                                                    ))}
+                                                </span>
+                                            </div>
+                                        )}
+                                        {data.created_by?.length>0 && (
+                                            <div className="info">
+                                                <span className="text bold">
+                                                    Creator:{" "}
+                                                </span>
+                                                <span className="text">
+                                                    {data.created_by?.map((d, i)=>(
+                                                        <span key={i}>{d.name}{data.created_by?.length-1 !==i && ", "}</span>
+                                                    ))}
+                                                </span>
+                                            </div>
+                                        )}
                                     </div>
                                 </div>
+                                <VideoPopup show={show} setShow={setShow} videoId={videoId} setVideoId={setVideoId}/>
                             </ContentWrapper>
                         </React.Fragment>
                     )}
